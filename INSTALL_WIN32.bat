9a4f4f22446494e84cbd64850a70d63ea3fa2744
guesdon <guesdon@24f41ba6-1239-4317-b21c-b79ed87a1d06>
2003-11-03 13:18:14 +0000

    Support for Win32

--- INSTALL_WIN32.bat
+++ INSTALL_WIN32.bat
@@ -1,40 +1,35 @@
-REM How I got OCamlODBC working under Win32
-REM by John Small ( jsmall@laser.net )
+@echo ocamlODBC installation script for Windows
+@echo Clement Capel Oct 2003
+@echo ----------------------------------------- 
 
-REM Get the OCamlODBC from
-REM http://www.maxence-g.net/tools_en.html
-REM http://www.maxence-g.net/ocamlodbc_2_2.tgz
+@echo Assumes VC++ installed 
+@echo variables set for "cl" and "link"
+@echo ----------------------------------------- 
 
-REM After unzipping ocamlodbc_2_2.tgz with WinZip
+@echo  Compile the external functions and create the dll
+cl /nologo /Ox /MD /DWIN32 -I %OCAMLLIB%\caml -c ocaml_odbc_c.c
+move ocaml_odbc_c.obj ocaml_odbc_c.d.obj
+link /nologo /dll /out:dllocamlodbc.dll /def:ocamlodbc.DEF  ocaml_odbc_c.d.obj %OCAMLLIB%\ocamlrun.lib  libodbc32.lib
+copy dllocamlodbc.dll %OCAMLLIB%\stublibs
 
-cd \ocaml\OcamlODBC\src
-
-REM  Assumes VC++ installed  (My vision is 6.0) and environment
-REM  variables set for "cl"
-
-REM  Copy and rename the odbc32.lib
-copy "\Program Files\Microsoft Visual Studio\Vc98\lib\Odbc32.lib" libodbc32.lib
-
-REM  Compile the external functions
-cl /c /DWIN32 /MT /I\ocaml\lib\caml ocaml_odbc_c.c
-
-REM  Make a native code library
+@echo  Make a native code library
 ocamlopt -a -o ocamlodbc.cmxa ocaml_odbc.ml ocamlodbc.mli ocamlodbc.ml ocaml_odbc_c.obj -cclib -lodbc32
-copy ocamlodbc.cmxa \ocaml\lib
-copy ocamlodbc.lib \ocaml\lib
-copy ocamlodbc.cmi \ocaml\lib
-copy libodbc32.lib \ocaml\lib
+copy ocamlodbc.cmxa %OCAMLLIB%
+copy ocamlodbc.cmi %OCAMLLIB%
 
-REM  Make a custom runtime library
-ocamlc -a -o ocamlodbc.cma -custom ocaml_odbc.ml ocamlodbc.mli ocamlodbc.ml ocaml_odbc_c.obj -cclib -lodbc32
-copy ocamlodbc.cma \ocaml\lib
-copy ocamlodbc.cmi \ocaml\lib
-copy libodbc32.lib \ocaml\lib
+@echo  Make a custom runtime library
+ocamlc -a -o ocamlodbc.cma -custom ocaml_odbc.ml ocamlodbc.mli ocamlodbc.ml ocaml_odbc_c.obj -dllib -locamlodbc  -cclib -lodbc32
+copy ocamlodbc.cma %OCAMLLIB%
+copy ocamlodbc.cmi %OCAMLLIB%
 
-REM  This runtime library is buggy and get the following error:
-REM  myprog.cmo and ocamlodbc.cma make inconsistent assumptions
-REM  over interface ocamlodbc.
 
-REM  But if I compile as shown below it's fine:
-REM  ocamlc -o myprog ocaml_odbc.ml ocamlodbc.mli libocaml.ml myprog.ml ocaml_odbc_c.obj -cclib -lodbc32
-REM  I'm guessing the bug is in how the ocamlodbc.cma file holds the interface declaration.
+
+
+
+
+
+
+
+
+
+
