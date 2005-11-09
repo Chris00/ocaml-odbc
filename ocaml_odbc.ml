2a1c34e3bba618a73ce1354e14c3ca166a757de7
chris <chris@24f41ba6-1239-4317-b21c-b79ed87a1d06>
2005-11-09 13:28:10 +0000

    ocamlodbc_ prefix; Documentation

--- ocaml_odbc.ml
+++ ocaml_odbc.ml
@@ -1,54 +1,84 @@
-(*********************************************************************************)
-(*                OCamlODBC                                                         *)
-(*                                                                               *)
-(*    Copyright (C) 2004 Institut National de Recherche en Informatique et       *)
-(*    en Automatique. All rights reserved.                                       *)
-(*                                                                               *)
-(*    This program is free software; you can redistribute it and/or modify       *)
-(*    it under the terms of the GNU Lesser General Public License as published   *)
-(*    by the Free Software Foundation; either version 2.1 of the License, or     *)
-(*    any later version.                                                         *)
-(*                                                                               *)
-(*    This program is distributed in the hope that it will be useful,            *)
-(*    but WITHOUT ANY WARRANTY; without even the implied warranty of             *)
-(*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *)
-(*    GNU Lesser General Public License for more details.                        *)
-(*                                                                               *)
-(*    You should have received a copy of the GNU Lesser General Public License   *)
-(*    along with this program; if not, write to the Free Software                *)
-(*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA                   *)
-(*    02111-1307  USA                                                            *)
-(*                                                                               *)
-(*    Contact: Maxence.Guesdon@inria.fr                                          *)
-(*********************************************************************************)
+(*****************************************************************************)
+(*              OCamlODBC                                                    *)
+(*                                                                           *)
+(*  Copyright (C) 2004 Institut National de Recherche en Informatique et     *)
+(*  en Automatique. All rights reserved.                                     *)
+(*                                                                           *)
+(*  This program is free software; you can redistribute it and/or modify     *)
+(*  it under the terms of the GNU Lesser General Public License as published *)
+(*  by the Free Software Foundation; either version 2.1 of the License, or   *)
+(*  any later version.                                                       *)
+(*                                                                           *)
+(*  This program is distributed in the hope that it will be useful,          *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *)
+(*  GNU Lesser General Public License for more details.                      *)
+(*                                                                           *)
+(*  You should have received a copy of the GNU Lesser General Public License *)
+(*  along with this program; if not, write to the Free Software              *)
+(*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA                 *)
+(*  02111-1307  USA                                                          *)
+(*                                                                           *)
+(*  Contact: Maxence.Guesdon@inria.fr                                        *)
+(*****************************************************************************)
 
-(* Definitions des types abstraits                       *)
+(** Low level part of OCamlODBC.  Do not use directly. *)
+
+(* Definitions des types abstraits *)
 
 type sQLHENV
+  (** Database environment *)
 type sQLHDBC
+  (** Database context *)
 type env
+  (** Result handle *)
 
 module type Sql_column_type =
-  sig
-    type t
-    val string : t -> string
-  end
+sig
+  type t
+  val string : t -> string
+end
 
 module Interface (Sql_col : Sql_column_type) =
-  struct
-    (*  Constructeurs des types abstraits (valeur vide) *)
-    external value_SQLHENV : unit -> sQLHENV = "value_HENV_c" 
-    external value_SQLHDBC : unit -> sQLHDBC = "value_HDBC_c"
+struct
+  (*  Constructeurs des types abstraits (valeur vide) *)
+  external value_SQLHENV : unit -> sQLHENV = "ocamlodbc_HENV_c"
+  external value_SQLHDBC : unit -> sQLHDBC = "ocamlodbc_HDBC_c"
 
-    (*  Fonctions C utilisées *)
-    external initDB : string -> string -> string -> (int*sQLHENV*sQLHDBC) = "initDB_c"
-    external initDB_driver : string -> bool -> (int*sQLHENV*sQLHDBC) = "initDB_driver_c"
-    external execDB : sQLHENV -> sQLHDBC -> string -> int * env = "execDB_c"
-    external itereDB : env -> int -> (int*string list list) = "itere_execDB_c"
-    external free_execDB : env -> unit = "free_execDB_c"
-    external get_infoDB : env -> sQLHENV -> sQLHDBC -> (string * Sql_col.t) list = "get_infoDB_c"
-    external exitDB : sQLHENV -> sQLHDBC -> int = "exitDB_c"
-  end
+  (*  Fonctions C utilisées *)
+  external initDB : string -> string -> string -> (int * sQLHENV * sQLHDBC)
+    = "ocamlodbc_initDB_c"
+    (** [initDB database user password] initializes a DB access.
+	@return (err, phEnv, phDbc) where [err <> 0] in case of error,
+	and [phEnv], [phDbc] are DB environment and context.  *)
+  external initDB_driver : string -> bool -> (int * sQLHENV * sQLHDBC)
+    = "ocamlodbc_initDB_driver_c"
+    (** [initDB_driver conn prompt] initializes a DB access where
+	[conn] is a ODBC driver connection string and [prompt = true]
+	if the driver should raise a dialog box to request username
+	and password.  The return value is the same as [initDB].  *)
+  external exitDB : sQLHENV -> sQLHDBC -> int
+    = "ocamlodbc_exitDB_c"
+    (** [exitDB phEnv phDbc] closes the DB access. *)
+
+  external execDB : sQLHENV -> sQLHDBC -> string -> int * env
+    = "ocamlodbc_execDB_c"
+    (** [execDB phEnv phDbc sql] retruns [(err, r)] where [r] is a
+	handle to the results of the [sql] statement and [err = 0] if
+	there was no error (in which case only [r] can be used).  *)
+  external free_execDB : env -> unit
+    = "ocamlodbc_free_execDB_c"
+    (** [free_execDB r] free the resources associated to the request
+	handle [r].  *)
+  external get_infoDB : env -> (string * Sql_col.t) list
+    = "ocamlodbc_get_infoDB_c"
+    (** [get_infoDB r] returns a list of pairs for each column in [r],
+	where the pair [(cn, t)] means that the column name is [cn]
+	and its SQL type is [t]. *)
+  external itereDB : env -> int -> (int * string list list)
+    = "ocamlodbc_itere_execDB_c"
+    (** [itereDB r n] returns a pair [(err, l)] where *)
+end
 
 
 
