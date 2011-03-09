0ccfec469ab945b3cbc251ac65b3b16f25b2598c
Maxence Guesdon <maxence.guesdon@inria.fr>
2011-03-09 17:38:32 +0100

    update headers

--- ocaml_odbc_c.c
+++ ocaml_odbc_c.c
@@ -1,26 +1,26 @@
-/*****************************************************************************/
-/*              OCamlODBC                                                    */
-/*                                                                           */
-/*  Copyright (C) 2004 Institut National de Recherche en Informatique et     */
-/*  en Automatique. All rights reserved.                                     */
-/*                                                                           */
-/*  This program is free software; you can redistribute it and/or modify     */
-/*  it under the terms of the GNU Lesser General Public License as published */
-/*  by the Free Software Foundation; either version 2.1 of the License, or   */
-/*  any later version.                                                       */
-/*                                                                           */
-/*  This program is distributed in the hope that it will be useful,          */
-/*  but WITHOUT ANY WARRANTY; without even the implied warranty of           */
-/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            */
-/*  GNU Lesser General Public License for more details.                      */
-/*                                                                           */
-/*  You should have received a copy of the GNU Lesser General Public License */
-/*  along with this program; if not, write to the Free Software              */
-/*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA                 */
-/*  02111-1307  USA                                                          */
-/*                                                                           */
-/*  Contact: Maxence.Guesdon@inria.fr                                        */
-/*****************************************************************************/
+/************************************************************************************/
+/*                OCamlODBC                                                         */
+/*                                                                                  */
+/*    Copyright (C) 2004 Institut National de Recherche en Informatique et          */
+/*    en Automatique. All rights reserved.                                          */
+/*                                                                                  */
+/*    This program is free software; you can redistribute it and/or modify          */
+/*    it under the terms of the GNU Lesser General Public License as published      */
+/*    by the Free Software Foundation; either version 2.1 of the License, or        */
+/*    any later version.                                                            */
+/*                                                                                  */
+/*    This program is distributed in the hope that it will be useful,               */
+/*    but WITHOUT ANY WARRANTY; without even the implied warranty of                */
+/*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                 */
+/*    GNU Lesser General Public License for more details.                           */
+/*                                                                                  */
+/*    You should have received a copy of the GNU Lesser General Public License      */
+/*    along with this program; if not, write to the Free Software                   */
+/*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA                      */
+/*    02111-1307  USA                                                               */
+/*                                                                                  */
+/*    Contact: Maxence.Guesdon@inria.fr                                             */
+/************************************************************************************/
 
 #ifndef lint
 static char vcid[]="$Id: ocaml_odbc_c.c,v 1.17 2008-03-28 03:11:47 chris Exp $";
